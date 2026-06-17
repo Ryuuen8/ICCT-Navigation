1a5531494f56fd2bcbbf791ee4c4d1db763d8ee8
Ryuuen8 <sabridobryan@gmail.com>
2026-06-17 09:04:53 +0800

    static

--- WebMap/WebMap/settings.py
+++ WebMap/WebMap/settings.py
@@ -125,18 +125,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATIC_ROOT = BASE_DIR / 'static'
-
-
-STATICFILES_DIRS = [
-    BASE_DIR / "Map/staticfiles",
-]
-from pathlib import Path
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Static files
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'Map' / 'static',
+]
+
+# Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
